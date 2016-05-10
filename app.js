232e21c2b305d66d141048a13e6d42980f6a7b06
Dan <dan@danj.eu>
2016-05-10 20:48:37 +0100

    Added pages for viewing users and exceptions

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.set('layout', 'layouts/default');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,9 +25,216 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
 
+app.get('/', function(req, res){
+  res.render('index', {
+    'title': 'Getting Started'
+  });
+});
+
+app.get('/setup-db', function(req, res){
+  res.render('setup-db', {
+    'title': 'Database Connection'
+  });
+});
+
+var url = null;
+
+app.post('/setup-db', function(req, res){
+  console.log(req.body);
+  //Now let's see if we can connect.
+  url = 'mongodb://'+req.body.host+':'+req.body.port+'/'+req.body.name;
+  console.log(url);
+  
+    MongoClient.connect(url, function(err, db){
+      if(err){
+        console.log(err);
+        res.render('setup-db', {
+          title: 'Database Connection',
+          failure: 'We couldn\'t connect to your database. Are you sure you entered the right details and it\'s running?'
+        });
+      }else{
+
+        res.redirect('/create-user');
+        //Now check if the Users collection exists.
+        // var users = db.collection('users');
+        // users.insertMany([
+          // {
+            // name: "Dan",
+            // email: "dan@danj.eu"
+          // }
+        // ], function(err, result){
+          // if(err){
+            // console.log('ERROR');
+            // console.log(err);
+          // }else{
+            // console.log('SUCCESS');
+            // console.log(result);
+          // }
+          // db.close();
+        // });
+
+        //db.close();
+
+
+      }
+    });
+  
+});
+
+app.get('/create-user', function(req, res){
+  
+  if(!url){
+    res.redirect('/');
+    return;
+  }
+
+  res.render('done', {
+          title: 'OMG!'
+        });
+});
+
+app.post('/create-user', function(req, res){
+  //Let's just make sure the DB connection still works.
+  console.log(url);
+  if(!url){
+    res.redirect('/');
+    return;
+  }
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      res.redirect('/');
+      return;
+    }
+
+    var errormsg = null;
+    if(req.body.password != req.body.confirmpassword){
+      errormsg = "Your passwords did not match";
+    }
+
+    if(req.body.name == ""){
+      errormsg = "Please enter a name";
+    }
+
+    if(req.body.email == ""){
+      errormsg = "Please enter an email";
+    }
+
+    if(errormsg){
+      res.render('done', {
+        title: 'OMG!',
+        formerror: errormsg,
+        oldname: req.body.name,
+        oldemail: req.body.email
+      });
+      return;
+    }
+
+    var users = db.collection('users');
+    users.insertMany([
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }
+      ], function(err, result){
+        if(err){
+          console.log('ERROR');
+          console.log(err);
+        }else{
+          console.log('SUCCESS');
+        }
+        });
+  });
+
+});
+
+app.get('/users', function(req, res){
+  if(!url){
+    res.redirect('/');
+    return;
+  }
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      res.redirect('/');
+      return;
+    }
+    var users = db.collection('users');
+
+    users.find({}).toArray(function(err, docs){
+      if(err){
+        console.log('none');
+      }else{
+        console.log(docs);
+        res.render('users', {
+          title: "Users",
+          users: docs
+        });
+      }
+    })
+  })
+});
+
+app.get('/exceptions', function(req, res){
+  if(!url){
+    res.redirect('/');
+    return;
+  }
+
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      res.redirect('/');
+      return;
+    }
+    var exceptions = db.collection('exceptions');
+
+    exceptions.find({}).toArray(function(err, docs){
+      if(err){
+        console.log('none');
+      }else{
+        res.render('exceptions', {
+          title: "Exceptions",
+          exceptions: docs
+        });
+      }
+    })
+  })
+});
+
+
+app.post('/api/v1/exception', function(req, res){
+
+  MongoClient.connect(url, function(err, db){
+    //console.log(req.body);
+    var exceptions = db.collection('exceptions');
+    exceptions.insertMany([
+      {
+        exception: req.body.exception,
+        message: req.body.message,
+        status: req.body.status,
+        code: req.body.code,
+        file: req.body.file,
+        line: req.body.line,
+        trace: req.body.trace
+      }
+    ], function(err, result){
+      if(err){
+        console.log(err);
+      }else{
+        console.log('lol');
+      }
+      db.close();
+      
+      });
+
+      
+    });
+  
+
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -40,7 +250,7 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
+      title: err.message,
       error: err
     });
   });
@@ -51,7 +261,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
+    title: err.message,
     error: {}
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	layout: 'layout.hjs',
+  	title: 'Getting Started',
+  	content: 'getting-started'
+
+  });
 });
 
-
-router.route('/api/v1/exception')
-
-.get(function(req, res) {
-
-	// This is for pulling out the information from the ID.
-	
-    
-})
-.post(function(req, res) {
-	console.log(req.body);
-
-	// So let's check if the data is formed correctly.
-	// If it is, push them into the database.
-
-});
 
 module.exports = router;
