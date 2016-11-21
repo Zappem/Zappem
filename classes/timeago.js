79e7d9c5bb306e4ca4f6012df889035ab46fdc69
Dan Johnson <dan@danj.eu>
2016-11-21 20:33:26 +0000

    Timestamps are a lot cleaner now, more like facebook

--- classes/timeago.js
+++ classes/timeago.js
@@ -3,26 +3,34 @@ module.exports = function(date) {
 
     var seconds = Math.floor((new Date() - date) / 1000);
 
-    var interval = Math.floor(seconds / 31536000);
+    // var interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) {
-        return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
+    // if (interval > 1) {
+    //     return interval + " years ago";
+    // }
+    // interval = Math.floor(seconds / 2592000);
+    // if (interval > 1) {
+    //     return interval + " months ago";
+    // }
+    // interval = Math.floor(seconds / 86400);
+    // if (interval > 1) {
+    //     return interval + " days ago";
+    // }
+    var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+
     interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
+    if (interval > 23) {
+        return date.getDate()+" "+month[date.getMonth()]+" "+date.getFullYear();
     }
-    interval = Math.floor(seconds / 60);
     if (interval > 1) {
-        return interval + " minutes ago";
+        return interval + " hrs";
     }
-    return Math.floor(seconds) + " seconds ago";
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " min";
+    }
+    if (interval > 0.1) {
+        return "< 1 min";
+    }
+    return "now";
 }
