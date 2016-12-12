e9db5e5e1d4a1dcbdc7b927ee1fbf932ceb04738
Dan Johnson <dan@danj.eu>
2016-12-12 23:29:15 +0000

    Fixed a typo

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ if(initChecks.isDataProvided()){
 				req.isConnected = app.isConnected;
 				res.locals.user = req.user;
 				if(config.demo) res.locals.demo = true;
-				res.locals.socketUrl = config.socketUrl;
+				res.locals.socketUrl = config.socket_url;
 				routes(req, res, next)
 			});
 			app.listen(process.env.PORT || 3000);
